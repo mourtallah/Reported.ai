6e8489301be8259cb5fb0bed1bbe674975dd76e8
mourtallah <serigne.faye@icloud.com>
2022-07-31 15:09:01 -0400

    correct imports

--- django/apps/tweets/admin.py
+++ django/apps/tweets/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import tweets
-from summer_proj_22.models import Tweets
+from .models import Tweets
 
 # Register your models here.
 admin.site.register(Tweets)
